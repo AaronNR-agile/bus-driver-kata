b19335d69bd8d4adb7ef26869a9c72c00cb0ec08
Aaron <axnobbyrajan@agiledigital.com.au>
2025-08-15 14:48:35 +1000

    Implementing rest of the helper functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,11 +2,15 @@ import { helloWorld } from ".";
 import fc from "fast-check";
 
 describe("saying hello", () => {
+
+  
   // Our special or important cases that are tested explicitly.
   it.each(["Jane", ""] as const)("returns the expected greeting", (name) => {
     const actual = helloWorld(name);
     expect(actual).toEqual(`Hello, ${name}!`);
   });
+
+
   // Our property based tests that cover a lot of ground, but no particular cases.
   it("should always include the name in the greeting", () => {
     fc.assert(
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-/**
- * Says hello.
- * @param name The person to say hello to.
- * @returns A greeting.
- */
-export const helloWorld = (name: string): string => `Hello, ${name}!`;
+//import { string } from "fast-check";
+
+type Route = ReadonlyArray<ReadonlyArray<number>>;
+type gossipKnowledge = Set<number>;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// export function main(input: string): number | "never" {
+//   let busRoutes: Route = createArrayFromString(input);
+
+//   return 1;
+// }
+
+export function createArrayFromString(input: string): number[][] {
+  return input.split("\n").map((x) => x.split(" ").map((x) => Number(x)));
+}
+
+export function initGossip(numberOfDrivers:number): gossipKnowledge[]{
+ return Array.from({length:numberOfDrivers},((_,indx)=> new Set<number>([indx])));
+}
+
+export function allBusDriversHaveGossiped(gossips: gossipKnowledge[], numberOfDrivers:number):boolean {
+    return gossips.every(x=>x.size === numberOfDrivers);
+}
+
+export function getCurrentStop(minute:number,routes:Route):number[] {
+  return []
+}
+
+console.log(createArrayFromString("3 1 2 3\n3 2 3 1\n4 2 3 4 5"));
+console.log(initGossip(createArrayFromString("3 1 2 3\n3 2 3 1\n4 2 3 4 5").length));
+console.log(allBusDriversHaveGossiped([new Set<number>([1,2]),new Set<number>([1,2])],2));
